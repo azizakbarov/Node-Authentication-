b41a34295c46830b1c662b6de9042f86d39fc027
Aziz Akbarov <azizbakbarov@gmail.com>
2021-11-23 13:06:58 +0500

    Using OAuth with Passport and Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
-}
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "440667630762209",
+    clientSecret: "7e4a86944074847c2d9da3ec465a0fc8",
+  },
+};
